7dcf6d1fc8926df1cc72474b82cac8c876e46168
Derek Foster <derek.foster@intel.com>
2024-05-23 19:11:49 -0700

    Extract ES2K NEIGHBOR_MOD_TABLE definitions
    - Extracted the LNWv3 NEIGHBOR_MOD_TABLE definitions into a
      separate header file.
    
    Signed-off-by: Derek Foster <derek.foster@intel.com>

--- switchapi/es2k/lnw_v2/switch_pd_p4_name_routing.h
+++ switchapi/es2k/lnw_v2/switch_pd_p4_name_routing.h
@@ -47,16 +47,6 @@
 
 #define LNW_ACTION_SET_SRC_MAC_PARAM_SRC_MAC_ADDR "arg"
 
-/* NEIGHBOR_MOD_TABLE */
-#define LNW_NEIGHBOR_MOD_TABLE "linux_networking_control.neighbor_mod_table"
-
-#define LNW_NEIGHBOR_MOD_TABLE_KEY_VENDORMETA_MOD_DATA_PTR \
-  "vmeta.common.mod_blob_ptr"
-
-#define LNW_NEIGHBOR_MOD_TABLE_ACTION_SET_OUTER_MAC \
-  "linux_networking_control.set_outer_mac"
-#define LNW_ACTION_SET_OUTER_MAC_PARAM_DST_MAC_ADDR "dst_mac_addr"
-
 /* RX_LAG_TABLE */
 #define LNW_RX_LAG_TABLE "linux_networking_control.rx_lag_table"
 
