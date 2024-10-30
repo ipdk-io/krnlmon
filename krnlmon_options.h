d330d9aef2c5af751eac713b7b6822ef449552d5
Derek Foster <derek.foster@intel.com>
2024-10-30 07:54:24 -0700

    Fix switchlink_link_test
    The switchlink_link unit test fails when run with the command
    
      bazel test --define target=dpdk --//flags:ovs=true //switchlink:all
    
    - Addressed the issue by conditionalizing out the two VxLAN test
      cases when OVSP4RT support is enabled.
    
    - Introduced an internal VXLAN_OPTION conditional to indicate
      whether VxLAN support is enabled.
    
    Signed-off-by: Derek Foster <derek.foster@intel.com>

--- krnlmon_options.h
+++ krnlmon_options.h
@@ -13,6 +13,17 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
+ *
+ * ---------------------------------------------------------------------
+ * This header file maps compile-time symbol definitions (such as
+ * ES2K_TARGET) to internal symbol definitions that specify which
+ * features the compile-time symbols enable (such as LAG_OPTION).
+ *
+ * This is infinitely better than leaving the reader scratching
+ * their head, trying to figure out why some random piece of code
+ * is wrapped in an "#if !defined(OVSP4RT_SUPPORT)" conditional
+ * (which the original author didn't bother to comment).
+ * ---------------------------------------------------------------------
  */
 
 #ifndef KRNLMON_OPTIONS_H_
@@ -20,13 +31,15 @@
 
 #if defined(DPDK_TARGET)
 // DPDK options
-
 #elif defined(ES2K_TARGET)
 // ES2K options
 #define LAG_OPTION 1
-
 #else
 #error "ASSERT: Unknown TARGET type!"
+#endif
+
+#if !defined(OVSP4RT_SUPPORT)
+#define VXLAN_OPTION 1
 #endif
 
 #endif  // KRNLMON_OPTIONS_H_
